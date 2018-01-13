0292cd28d6b485d88ab15837517158dba3b94b32
Mattias Hammarhorn <mattias@hammarhorn.se>
2018-01-13 17:33:57 +0100

    Added create, list-posts-methods for Thread.

---
+++ Web_Forum/Controllers/ContentsController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Web_Forum.Entities;
+using Microsoft.AspNetCore.Identity;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Web_Forum.Controllers
+{
+    [Route("contents")]
+    public class ContentsController : Controller
+    {
+        private UserManager<ApplicationUser> userManager;
+        private SignInManager<ApplicationUser> signInManager;
+        private Web_ForumDbContext web_ForumDbContext;
+
+        public ContentsController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, Web_ForumDbContext web_ForumDbContext)
+        {
+            this.web_ForumDbContext = web_ForumDbContext;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost, Route("/contents")]
+        async public Task<IActionResult> SaveThreadAndPostToDatabase(Thread UserCreatedThread, Post UserCreatdedOriginalPost)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                UserCreatedThread.DateOfCreation = DateTime.Now.ToString();
+
+                web_ForumDbContext.Add(UserCreatedThread);
+                await web_ForumDbContext.SaveChangesAsync();
+
+                UserCreatdedOriginalPost.CreatedBy = User.Identity.Name;
+                UserCreatdedOriginalPost.DateOfCreation = DateTime.Now.ToString();
+                UserCreatdedOriginalPost.ThreadId = UserCreatedThread.Id;
+
+                web_ForumDbContext.Add(UserCreatdedOriginalPost);
+
+                await web_ForumDbContext.SaveChangesAsync();
+
+                return Ok(UserCreatedThread);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet, Route("/contents/threads")]
+        public IActionResult GetThread(int id)
+        {
+            var result = web_ForumDbContext.Threads.SingleOrDefault(t => t.Id == id);
+
+            return Ok(result);
+        }
+
+        [HttpGet, Route("/contents/threads/{id:int}/posts")]
+        public IActionResult GetThreadPosts(int id)
+        {
+            var result = web_ForumDbContext.Posts.Where(p => p.ThreadId == id);
+
+            return Ok(result);
+        }
+
+        //[HttpDelete, Route("/contents/threads/{id}")]
+        //public IActionResult DeleteThread(int id)
+        //{
+        //    var threadToDelete = web_ForumDbContext.Threads.SingleOrDefault(t => t.Id == id);
+
+        //    var result = web_ForumDbContext.Threads.Remove(threadToDelete);
+
+        //    return Ok(result);
+        //}
+
+        //[HttpPut, Route("/contents/threads/{id}")]
+        //public IActionResult UpdateThread(Thread ThreadToUpdate)
+        //{
+        //    var threadToUpdate = web_ForumDbContext.Threads.SingleOrDefault(t => t.Id == ThreadToUpdate.Id);
+
+        //    threadToUpdate.Title = threadToUpdate.Title;
+        //    threadToUpdate.DateOfLastUpdate = DateTime.Now.ToString();
+
+
+        //    return Ok("");
+        //}
+
+        [HttpGet, Route("/contents/getAllThreads")]
+        public IActionResult GetAllThreads()
+        {
+            List<Thread> result = web_ForumDbContext.Threads.ToList();
+
+            result.Sort((a, b) => b.DateOfCreation.CompareTo(a.DateOfCreation));
+
+            return Ok(result);
+        }
+
+        //[HttpGet, Route("")]
+    }
+}
--- Web_Forum/wwwroot/js/Web_Forum.js
+++ Web_Forum/wwwroot/js/Web_Forum.js
@@ -1,9 +1,9 @@
 ﻿$(document).ready(function () {
     updateNavBar();
+    updateThreadDiv();
 });
 //test
-function updateNavBar()
-{
+function updateNavBar() {
     $.ajax({
         url: '/user/checkIfUserIsAuthenticated',
         method: 'GET'
@@ -107,7 +107,6 @@ $("#createPostForm button").click(function () {
         method: 'POST',
         data: {
             "Content": $("#createPostForm [name=CreatePost]").val()
-
         }
 
     })
@@ -133,14 +132,14 @@ $("#showTestPostsButton button").click(function () {
         url: '/user/showallposts',
         method: 'GET',
         data: {
-            
+
         }
 
     })
         .done(function (result) {
             var list = ''
             for (i = 0; i < result.length; i++) {
-                list += "<p style='border:3px; border-style:solid; border-color:#FF0000; padding:1em;' > " + result[i].content + " Skapad av: " + result[i].createdBy + " Klockan " + result[i].dateOfCreation +"<p>"+'<br>';
+                list += "<p style='border:3px; border-style:solid; border-color:#FF0000; padding:1em;' > " + result[i].content + " Skapad av: " + result[i].createdBy + " Klockan " + result[i].dateOfCreation + "<p>" + '<br>';
             };
             $('#showTestPosts').html(list);
 
@@ -156,3 +155,139 @@ $("#showTestPostsButton button").click(function () {
         })
 
 });
+
+$("#createThreadAndPostForm button").click(function () {
+
+    $.ajax({
+        url: '/contents/',
+        method: 'POST',
+        data: {
+            "Title": $("#createThreadAndPostForm [name=CreateThreadTitle]").val(),
+            "Content": $("#createThreadAndPostForm [name=CreatePostContent]").val()
+        }
+    })
+        .done(function (result) {
+            updateThreadDiv();
+            console.log(result);
+        })
+
+        .fail(function (xhr, status, error) {
+            alert("fail!");
+            console.log("Error", xhr, status, error)
+        });
+});
+
+function updateThreadDiv() {
+
+    $.ajax({
+        url: '/contents/getAllThreads',
+        method: 'GET'
+    })
+        .done(function (result) {
+
+            var threads = fillTableWithThreads(result);
+            $("#threadDiv tbody").empty();
+            $("#threadDiv tbody").append(threads);
+
+            console.log(result);
+        })
+
+        .fail(function (xhr, status, error) {
+            alert("fail!");
+            console.log("Error", xhr, status, error)
+        });
+
+}
+
+function fillTableWithThreads(result) {
+
+    var html = "";
+
+    $.each(result, function (key, thread) {
+        html += '<tr>';
+        html += '<td style="border: 1px solid black;">' + '<a href="#threadDataDiv" class="threadLink" thread-id="' + thread.id + '">' + thread.title + '</a>' + '</td>';
+        html += '<td style="border: 1px solid black;">' + thread.dateOfCreation + '</td>';
+        html += '<td style="border: 1px solid black;">' + thread.amountOfReplies + '</td>';
+        html += '<td style="border: 1px solid black;">' + thread.amountOfViews + '</td>';
+        html += '</tr>';
+    });
+
+    return html;
+}
+
+$(document).on("click", "a.threadLink", function () {
+
+    var id = $(this).attr("thread-id");
+
+    $.ajax({
+        url: '/contents/threads/',
+        method: 'GET',
+        data: {
+            "id": id
+        }
+    })
+        .done(function (result) {
+            buildThreadDataTable(result);
+            fillTableWithThreadPosts(result);
+
+            console.log(result);
+        })
+
+        .fail(function (xhr, status, error) {
+            alert("fail!");
+            console.log("Error", xhr, status, error)
+        });
+
+});
+
+function buildThreadDataTable(result) {
+    emptythreadDataDiv();
+
+    var html = '<a href="/index.html">' + 'Tillbaka' + '</a>' + ' > ' + result.title;
+    html += '<h1>' + result.title + '</h1>';
+
+    $("#threadDataDiv").append(html);
+}
+
+function fillTableWithThreadPosts(result) {
+
+    var id = result.id;
+    console.log(id);
+
+    $.ajax({
+        url: '/contents/threads/' + id + '/posts',
+        method: 'GET',
+        data: {
+            "id": id
+        }
+    })
+        .done(function (result) {
+
+            var html = "";
+
+            $.each(result, function (key, post) {
+                html += '<tr>';
+                html += '<td style="border: 1px solid black;">' + post.createdBy + '</td>';
+                html += '<td style="border: 1px solid black;">' + post.dateOfCreation + '</td>';
+                html += '<td style="border: 1px solid black;">' + post.content + '</td>';
+                html += '<td><button class="deletePostButton_' + post.id + '">Delete</button></td>';
+                html += '<td><button class="editPostButton_' + post.id + '">Edit</button></td>';
+                html += '</tr>';
+            });
+
+            $("#threadDataDiv").append(html);
+
+            console.log(result);
+        })
+
+        .fail(function (xhr, status, error) {
+            alert("fail!");
+
+            console.log("Error", xhr, status, error)
+        });
+
+}
+
+function emptythreadDataDiv() {
+    $("#threadDataDiv").empty();
+}
